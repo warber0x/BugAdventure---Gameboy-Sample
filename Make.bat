7d3f6ec1798be5e62ed7592b25d05c2f9e6a7c4f
TheR3d0ne <sam.rad@hotmail.fr>
2021-12-19 22:15:20 +0100

    Update Make.bat

--- Make.bat
+++ Make.bat
@@ -1,4 +1,4 @@
 del main.o
 del %2
-"C:\Users\samra\OneDrive\Bureau\Personal Files\Gameboy developpment\GB Dev 2020 + Debuggers\gbdk2020\bin\lcc" -Wa-l -Wl-m -Wf--debug -Wl-y -Wl-w -c -o main.o %1
-"C:\Users\samra\OneDrive\Bureau\Personal Files\Gameboy developpment\GB Dev 2020 + Debuggers\gbdk2020\bin\lcc" -Wa-l -Wl-m -Wf--debug -Wl-y -Wl-w -o %2 main.o
+"Gameboy developpment\GB Dev 2020 + Debuggers\gbdk2020\bin\lcc" -Wa-l -Wl-m -Wf--debug -Wl-y -Wl-w -c -o main.o %1
+"Gameboy developpment\GB Dev 2020 + Debuggers\gbdk2020\bin\lcc" -Wa-l -Wl-m -Wf--debug -Wl-y -Wl-w -o %2 main.o
